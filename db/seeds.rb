337e5b3f89e73132175a94fac1af68e11fcb0a4a
codehass <hassan.elouardy06@gmail.com>
2023-05-04 01:20:11 +0000

    Add data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+g1 = Greeting.create(name: 'Hello this is greeting 1')
+g2 = Greeting.create(name: 'Hello this is greeting 2')
+g3 = Greeting.create(name: 'Hello this is greeting 3')
+g4 = Greeting.create(name: 'Hello this is greeting 4')
+g5 = Greeting.create(name: 'Hello this is greeting 5') 
